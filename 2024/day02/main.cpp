dc9c0aa795c7eb08d123685b91d1ba0f4533da33
itsVerix <weronika.demich@gmail.com>
2024-12-02 22:54:25 +0100

    02/2024: Part 2

--- 2024/day02/main.cpp
+++ 2024/day02/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include <fstream>
-#include <list>
 #include <sstream>
+#include <vector>
+#include <cmath>
+#include <string>
+
+bool is_safe(const std::vector<int>& numbers) {
+    if (numbers.size() < 2) {
+        return true;
+    }
+    const bool is_increasing = numbers.at(0) <= numbers.at(1);
+    for (int i = 0; i < numbers.size() - 1; i++) {
+        int difference = numbers.at(i + 1) - numbers.at(i);
+        if ((is_increasing && difference < 0) || (!is_increasing && difference > 0) || std::abs(difference) > 3 || difference == 0) {
+            return false;
+        }
+    }
+    return true;
+}
 
 int main() {
     std::ifstream input("input.txt", std::ifstream::in);
@@ -21,20 +37,21 @@ int main() {
             numbers.push_back(number);
         }
 
-        bool is_increasing = true;
-        bool is_report_safe = true;
-        if (numbers.at(0) > numbers.at(1))
-            is_increasing = false;
+        bool is_report_safe = is_safe(numbers);
 
-        for (int i = 0; i < numbers.size() - 1; i++) {
-            int difference = numbers.at(i + 1) - numbers.at(i);
-
-            if ((difference < 0 && is_increasing) || (difference > 0 && !is_increasing) || std::abs(difference) > 3 || difference == 0) {
-                is_report_safe = false;
-                break;
+        if (!is_report_safe) {
+            for (int i = 0; i < numbers.size(); i++) {
+                std::vector<int> modified_numbers = numbers;
+                modified_numbers.erase(modified_numbers.begin() + i);
+                if (is_safe(modified_numbers)) {
+                    is_report_safe = true;
+                    break;
+                }
             }
         }
         if (is_report_safe) safe_reports_cnt++;
     }
-    std::cout << safe_reports_cnt << std::endl;
+
+    std::cout << "Total Safe Reports: " << safe_reports_cnt << std::endl;
+    return 0;
 }
